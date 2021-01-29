cfa09ba8cfc86e2ef07fc59130fff9730ee54131
briouyaasmae <briouya.asmae@gmail.com>
2021-01-29 12:09:55 +0100

    new project spring

---
+++ src/test/java/com/form/org/EnsapaycmiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.form.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsapaycmiApplicationTests {
+
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
